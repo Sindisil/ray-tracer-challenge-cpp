44a31fe86ee4130f7524075ce1b641069c01f4bb
sindisil <greg.jandl@gmail.com>
2020-11-18 21:29:30 -0600

    Move Matrix and its tests into their own files.

---
+++ src/matrix.h
@@ -0,0 +1,177 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include "primatives.h"
+
+namespace raytrace {
+template <size_t R, size_t C> class Matrix {
+public:
+  Matrix(std::initializer_list<float> vals) {
+    static_assert(R > 1 || C > 1,
+                  "At least one dimension of a Matrix must be greater than 1");
+    int i = 0;
+    for (auto vi = std::begin(vals); vi != std::end(vals) && i < m_cells.size();
+         ++vi) {
+      m_cells[i] = *vi;
+      ++i;
+    }
+  }
+
+  float &operator()(int r, int c) {
+    if (r < 0 || c < 0 || r >= R || c >= C) {
+      throw std::out_of_range("index out of range");
+    }
+    return m_cells.at(r * C + c);
+  }
+
+  Matrix<R, C> transpose() {
+    Matrix<R, C> t{};
+    for (int c = 0; c < C; ++c) {
+      for (int r = 0; r < C; ++r) {
+        t(c, r) = m_cells[r * C + c];
+      }
+    }
+    return t;
+  }
+
+  Matrix<R - 1, C - 1> submatrix(int r_skip, int c_skip) {
+    static_assert(R > 1, "Result of submatrix must have at least one row");
+    static_assert(C > 1, "Result of submatrix must have at least one column");
+    Matrix<R - 1, C - 1> s{};
+    int srow = 0;
+    int scol = 0;
+
+    for (int r = 0; r < R; ++r) {
+      if (r != r_skip) {
+        for (int c = 0; c < C; ++c) {
+          if (c != c_skip) {
+            s(srow, scol) = m_cells[r * C + c];
+            ++scol;
+          }
+        }
+        ++srow;
+        scol = 0;
+      }
+    }
+    return s;
+  }
+
+  bool is_invertable() { return R == C && determinant() != 0; }
+
+  Matrix<R, C> inverse() {
+    if (!is_invertable()) {
+      throw std::domain_error("Matrix not invertable");
+    }
+
+    Matrix<R, C> m{};
+    for (int row = 0; row < R; ++row) {
+      for (int col = 0; col < C; ++col) {
+        auto c = cofactor(row, col);
+        m(col, row) = c / determinant();
+      }
+    }
+    return m;
+  }
+
+  float minor(int r, int c) {
+    static_assert(R == C, "Can't find find the minor of non-square matrix");
+    return submatrix(r, c).determinant();
+  }
+
+  float cofactor(int r, int c) {
+    static_assert(R == C, "Can't find cofactor of non-square matrix");
+    auto f = minor(r, c);
+    return (r + c) % 2 == 0 ? f : -f;
+  }
+
+  float determinant() {
+    static_assert(R == C, "Can't find determinant of non-square matrix");
+    if constexpr (R == 2) {
+      return ((*this)(0, 0) * (*this)(1, 1)) - ((*this)(0, 1) * (*this)(1, 0));
+    } else {
+      float det = 0;
+      for (int c = 0; c < R; ++c) {
+        det += (*this)(0, c) * cofactor(0, c);
+      }
+      return det;
+    }
+  }
+
+private:
+  std::array<float, R * C> m_cells{0};
+};
+
+template <size_t R, size_t C>
+bool operator==(Matrix<R, C> lhs, Matrix<R, C> rhs);
+
+template <size_t R, size_t C>
+inline bool operator!=(Matrix<R, C> lhs, Matrix<R, C> rhs) {
+  return !(lhs == rhs);
+};
+
+template <size_t R, size_t C, size_t N>
+Matrix<R, C> operator*(Matrix<R, N> lhs, Matrix<N, C> rhs);
+
+template <size_t N> Matrix<N, N> identity_matrix() {
+  Matrix<N, N> m{};
+  for (int i = 0; i < static_cast<int>(N); ++i) {
+    m(i, i) = 1;
+  }
+  return m;
+}
+
+template <size_t R, size_t C> Point operator*(Matrix<R, C> lhs, Point p);
+template <size_t R, size_t C> Vec3 operator*(Matrix<R, C> lhs, Vec3 v);
+
+template <size_t R, size_t C>
+bool operator==(Matrix<R, C> lhs, Matrix<R, C> rhs) {
+  for (int r = 0; r < R; ++r) {
+    for (int c = 0; c < C; ++c) {
+      if (!about_equal(lhs(r, c), rhs(r, c))) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+template <size_t R, size_t C, size_t N>
+Matrix<R, C> operator*(Matrix<R, N> lhs, Matrix<N, C> rhs) {
+  Matrix<R, C> res{};
+
+  for (int r = 0; r < static_cast<int>(R); ++r) {
+    for (int c = 0; c < static_cast<int>(C); ++c) {
+      for (int n = 0; n < static_cast<int>(N); ++n) {
+        res(r, c) += lhs(r, n) * rhs(n, c);
+      }
+    }
+  }
+  return res;
+}
+
+template <size_t R, size_t C> Point operator*(Matrix<R, C> lhs, Point p) {
+  Matrix<4, 1> rhs{p.x, p.y, p.z, 0};
+  Matrix<4, 1> prod = lhs * rhs;
+  return Point{prod(0, 0), prod(1, 0), prod(2, 0)};
+}
+template <size_t R, size_t C> Vec3 operator*(Matrix<R, C> lhs, Vec3 v) {
+  Matrix<4, 1> rhs{v.x, v.y, v.z, 1};
+  Matrix<4, 1> prod = lhs * rhs;
+  return Vec3{prod(0, 0), prod(1, 0), prod(2, 0)};
+}
+
+template <size_t R, size_t C>
+std::ostream &operator<<(std::ostream &os, Matrix<R, C> const &val) {
+  Matrix<R, C> &m = const_cast<Matrix<R, C> &>(val);
+  for (int r = 0; r < static_cast<int>(R); ++r) {
+    os << (r == 0 ? "" : ", ") << "(";
+    for (int c = 0; c < static_cast<int>(C); ++c) {
+      os << (c == 0 ? "" : ", ") << m(r, c);
+    }
+    os << ")";
+  }
+  return os;
+}
+
+} // namespace raytrace
+#endif // !MATRIX_H
