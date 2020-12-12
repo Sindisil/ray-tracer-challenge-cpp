41c1416f1312353f59a8ef00d07d71fa7984d616
sindisil <greg.jandl@gmail.com>
2020-12-12 14:50:34 -0600

    Default to white light & add ==, !=, and  << ops

--- include/lights.h
+++ include/lights.h
@@ -1,15 +1,31 @@
-#ifndef RAYTRACE_LIGHTS_H
-#define RAYTRACE_LIGHTS_H
+#ifndef RAYTRACE_LIGHTS_H_GUARD
+#define RAYTRACE_LIGHTS_H_GUARD
 
 #include "color.h"
 #include "primitives.h"
 
+#include <ostream>
+
 namespace raytrace {
 
 struct PointLight {
-  Point position;
-  Color intensity;
+  Point position{0.0f, 0.0f, 0.0f};
+  Color intensity{colors::white};
+
+  friend auto operator==(PointLight lhs, PointLight rhs) {
+    return lhs.position == rhs.position && lhs.intensity == rhs.intensity;
+  }
+
+  friend auto operator!=(PointLight lhs, PointLight rhs) {
+    return !(lhs == rhs);
+  }
 };
+
+inline auto operator<<(std::ostream &os, PointLight const &val)
+    -> std::ostream & {
+  os << "PointLight(" << val.intensity << " @ " << val.position << ")";
+  return os;
+}
 
 } // namespace raytrace
 #endif
--- tests/test_lights.cpp
+++ tests/test_lights.cpp
@@ -2,6 +2,9 @@
 
 #include "doctest.h"
 
+#include "color.h"
+#include "primitives.h"
+
 using raytrace::Color;
 using raytrace::Point;
 using raytrace::PointLight;
@@ -12,4 +15,19 @@ TEST_CASE("A point light has position and intensity") {
   auto light = PointLight{position, intensity};
   REQUIRE(light.intensity == intensity);
   REQUIRE(light.position == position);
+}
+
+TEST_CASE("A default constructed PointLight is a white light at {0, 0, 0}") {
+  auto l = PointLight{};
+  CHECK(l.intensity == raytrace::colors::white);
+  CHECK(l.position == Point{0.0f, 0.0f, 0.0f});
+}
+
+TEST_CASE("PointLights can be compared") {
+  auto p1 = PointLight{Point{1, 1, 1}, Color{1, 2, 3}};
+  auto p2 = PointLight{Point{1, 1, 1}, Color{1, 2, 3}};
+  auto p3 = PointLight{Point{0, 0, 0}, Color{1, 1, 1}};
+
+  CHECK(p1 == p2);
+  CHECK(p1 != p3);
 }
