19bc9905841b6d1f179be19dc4d9436c71a5c90a
sindisil <greg.jandl@gmail.com>
2020-12-15 21:21:02 -0600

    Implement view_transform function

---
+++ include/transformations.h
@@ -0,0 +1,20 @@
+#ifndef RAYTRACE_TRANSFORMATIONS_H_GUARD
+#define RAYTRACE_TRANSFORMATIONS_H_GUARD
+
+#include "matrix.h"
+#include "primitives.h"
+
+namespace raytrace {
+
+inline auto view_transform(Point from, Point to, Vec3 up) -> Matrix<4> {
+  auto forward = (to - from).normalize();
+  auto left = cross(forward, up.normalize());
+  auto true_up = cross(left, forward);
+  auto orientation = Matrix<4>{{{left.x, left.y, left.z, 0.0f},
+                                {true_up.x, true_up.y, true_up.z, 0.0f},
+                                {-forward.x, -forward.y, -forward.z, 0.0f},
+                                {0.0f, 0.0f, 0.0f, 1.0f}}};
+  return orientation * identity_matrix().translate(-from.x, -from.y, -from.z);
+}
+} // namespace raytrace
+#endif
